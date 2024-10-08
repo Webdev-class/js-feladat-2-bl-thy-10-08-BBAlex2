b5d0d0c836928929d2ce69bb58d660c6efa7ec6a
BBAlex2 <badaribekesialex2@gmail.com>
2024-10-08 20:52:02 +0000

    waahoo

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,19 @@
 
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const APIAddress = 'https://archive-api.open-meteo.com/v1/archive?';
 async function f1 (lat, long, timeStr) {
-    //TODO
+    xhttp.open("GET",`${APIAddress}latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`);
+    xhttp.onload = function(load){
+        data = JSON.parse(this.responseText);
+
+        let tempSum = 0;
+        data.hourly.temperature_2m.forEach((num) => {
+            tempSum += num;
+        });
+
+        let tempAvg = Math.floor(tempSum / 24);
+        console.log(tempAvg);
+    }
+    xhttp.send();
 }
 
 module.exports = f1;
--- f2/f2.js
+++ f2/f2.js
@@ -3,14 +3,16 @@ Készítsd el az előző feladat azon változatát ahol megadjuk az óraszámot 
 Csak az adott órára vonatkozó hőmérséklettel térjen vissza a függvény.
 */
 
-const locations = require('../locations.json');
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const APIAddress = 'https://archive-api.open-meteo.com/v1/archive?';
+async function f2(lat, long, timeStr, hour){
+    xhttp.open("GET",`${APIAddress}latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`);
+    xhttp.onload = function(load){
+        data = JSON.parse(this.responseText);
 
-const f2 = async (lat, long, timeStr, hour) => {
-    const str = APIAdress + `latitude=${lat}&longitude=${long}` +
-        `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
-    const rArr = await fetch(str)
-        .then(data => data.json());
-    return rArr.hourly.temperature_2m.at(hour);
+        let tempHour = data.hourly.temperature_2m[hour-1];
+        console.log(tempHour);
+    }
+    xhttp.send();
 }
+
 module.exports = f2;
